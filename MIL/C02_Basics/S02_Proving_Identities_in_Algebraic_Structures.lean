a9e2d634187684fa755dde9bdcb68fc30da48ba0
Benjamin F Jones <benjaminfjones@gmail.com>
2023-06-23 23:22:54 -0700

    Finish proofs in C02_S02

--- MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
+++ MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
@@ -53,13 +53,17 @@ theorem neg_add_cancel_left (a b : R) : -a + (a + b) = b := by
 
 -- Prove these:
 theorem add_neg_cancel_right (a b : R) : a + b + -b = a := by
-  sorry
+  rw [add_assoc, add_comm b _, add_left_neg, add_zero]
 
 theorem add_left_cancel {a b c : R} (h : a + b = a + c) : b = c := by
-  sorry
+  rw [← zero_add b, ← add_left_neg a, add_assoc]
+  rw [h]
+  rw [neg_add_cancel_left]
 
 theorem add_right_cancel {a b c : R} (h : a + b = c + b) : a = c := by
-  sorry
+  rw [← add_zero a, ← add_left_neg b, add_comm (-b) _, ← add_assoc]
+  rw [h]
+  rw [add_neg_cancel_right]
 
 theorem mul_zero (a : R) : a * 0 = 0 := by
   have h : a * 0 + a * 0 = a * 0 + 0 := by
@@ -67,20 +71,27 @@ theorem mul_zero (a : R) : a * 0 = 0 := by
   rw [add_left_cancel h]
 
 theorem zero_mul (a : R) : 0 * a = 0 := by
-  sorry
+  have h : 0 * a + 0 * a = 0 * a + 0 := by
+    rw [← add_mul, add_zero, add_zero]
+  apply add_left_cancel h
 
 theorem neg_eq_of_add_eq_zero {a b : R} (h : a + b = 0) : -a = b := by
-  sorry
+  have ha : -a + a = 0 := add_left_neg a
+  rw [← h, add_comm (-a) a] at ha
+  exact add_left_cancel ha
 
 theorem eq_neg_of_add_eq_zero {a b : R} (h : a + b = 0) : a = -b := by
-  sorry
+  have hb : -b + b = 0 := add_left_neg b
+  rw [← hb, ] at h
+  exact add_right_cancel h
 
 theorem neg_zero : (-0 : R) = 0 := by
   apply neg_eq_of_add_eq_zero
   rw [add_zero]
 
 theorem neg_neg (a : R) : - -a = a := by
-  sorry
+  apply neg_eq_of_add_eq_zero
+  exact add_left_neg a
 
 end MyRing
 
@@ -102,14 +113,15 @@ example (a b : ℝ) : a - b = a + -b := by
 namespace MyRing
 variable {R : Type _} [Ring R]
 
-theorem self_sub (a : R) : a - a = 0 :=
-  sorry
+theorem self_sub (a : R) : a - a = 0 := by
+  rw [sub_eq_add_neg]
+  exact add_right_neg _
 
 theorem one_add_one_eq_two : 1 + 1 = (2 : R) := by
   norm_num
 
-theorem two_mul (a : R) : 2 * a = a + a :=
-  sorry
+theorem two_mul (a : R) : 2 * a = a + a := by
+  rw [← one_add_one_eq_two, add_mul, one_mul]
 
 end MyRing
 
@@ -131,14 +143,57 @@ variable {G : Type _} [Group G]
 
 namespace MyGroup
 
-theorem mul_right_inv (a : G) : a * a⁻¹ = 1 := by
-  sorry
+-- helper, didn't end up needing this
+theorem inv_mul_cancel_left (a b : G) : a⁻¹ * (a * b) = b := by
+  rw [← mul_assoc, mul_left_inv, one_mul]
 
+-- helper
+theorem mul_left_cancel {a b c : G} (h : a * b = a * c) : b = c := by
+  rw [← one_mul b, ← mul_left_inv a, mul_assoc]
+  rw [h]
+  rw [← mul_assoc, mul_left_inv, one_mul]
+
+-- helper for `mul_one`
+theorem eq_of_inv_mul_eq_one (a b : G) (h : a⁻¹ * b = 1) : a = b := by
+  have h1 : a⁻¹ * a = 1 := mul_left_inv a
+  rw [← h] at h1
+  exact mul_left_cancel h1
+
+-- Exercise 2
 theorem mul_one (a : G) : a * 1 = a := by
-  sorry
+  have h1 : (1 : G) * 1 = 1 := one_mul 1
+  nth_rw 1 [← mul_left_inv a] at h1
+  rw [mul_assoc] at h1
+  have h2 : a = a * 1 := eq_of_inv_mul_eq_one a (a * 1) h1
+  rw [← h2]
 
+-- Exercise 1
+theorem mul_right_inv (a : G) : a * a⁻¹ = 1 := by
+  have h : 1 * a⁻¹ = a⁻¹ := by rw [one_mul]
+  rw [← mul_left_inv a, mul_assoc] at h
+  nth_rw 3 [← mul_one a⁻¹] at h
+  exact mul_left_cancel h
+
+-- helper
+theorem eq_inv_of_mul_one (a b : G) (h : a * b = 1) : a = b⁻¹ :=
+  calc
+    a = a * 1 := Eq.symm (mul_one a)
+    _ = a * (b * b⁻¹) := by rw [← mul_right_inv b]
+    _ = (a * b) * b⁻¹ := by rw [← mul_assoc]
+    _ = 1 * b⁻¹ := by rw [h]
+    _ = b⁻¹ := by rw [one_mul]
+
+-- Exercise 3
 theorem mul_inv_rev (a b : G) : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by
-  sorry
+  -- apply that x * g = 1 => x = g⁻¹
+  have h : (b⁻¹ * a⁻¹) * (a * b) = 1 := by
+    calc
+      (b⁻¹ * a⁻¹) * (a * b) = ((b⁻¹ * a⁻¹) * a) * b := by rw [← mul_assoc]
+      _                     = (b⁻¹ * (a⁻¹ * a)) * b := by rw [mul_assoc b⁻¹ _ _]
+      _                     = (b⁻¹ * 1) * b         := by rw [mul_left_inv]
+      _                     = b⁻¹ * b               := by rw [mul_one]
+      _                     = 1                     := by rw [mul_left_inv]
+  exact Eq.symm (eq_inv_of_mul_one (b⁻¹ * a⁻¹) (a * b) h)
 
 end MyGroup
 
